5c43f8dbac7700f3bce8704d74dc1d13a9c2a6c5
theGeekyLad <rahul.pillai03@gmail.com>
2021-07-25 23:19:09 +0530

    Added more websites to config

--- hopsearch-server/hopsearch-server.js
+++ hopsearch-server/hopsearch-server.js
@@ -7,7 +7,14 @@ app.get('/', (req, res) => {
     res.send(JSON.stringify([{
         'stackoverflow.com': '.s-input',
         'www.linkedin.com': '.search-global-typeahead__input',
-        'www.windy.com': '#q'
+        'www.windy.com': '#q',
+        'www.vocabulary.com': '#search',
+        'www.hdfcbank.com': '.dummy-input',
+        "developer.chrome.com": ".search-box__input",
+        "www.amazon.in": "#twotabsearchtextbox",
+        "www.flipkart.com": "._3704LK",
+        "www.quora.com": ".q-input",
+        "www.reddit.com": "#header-search-bar"
     }]));
 })
 
